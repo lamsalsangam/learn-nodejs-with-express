54b6eed631f0f9a91b854df8f47c5613d518489f
unknown <me.sangam.lamsal.10@gmail.com>
2023-01-16 08:45:30 +0545

    BMI Calculator app with node server

---
+++ BMI Calculator/bmi_calculator.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function (req, res) {
+  let weight = Number(req.body.weight)
+  let height = Number(req.body.height)
+
+  let result = weight / height ** 2
+
+  res.send(`Your BMI calculated BMI is <h4>${result}</h4>`)
+})
+
+app.listen(3000, function () {
+  console.log('Your server is starting on the port 3000')
+})
